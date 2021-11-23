ba520efc072ad257b3b9d7dc3156bb28d7e55814
garethpark <garethpark@yahoo.com>
2021-11-23 12:39:09 +0000

    skeleton test methods

--- src/main/java/mchall/Rover.java
+++ src/main/java/mchall/Rover.java
@@ -2,10 +2,31 @@ package mchall;
 
 public class Rover {
 
-    public Rover (int x, int y, String direction){
+    private static final String MOVE_COMMAND = "Move" ;
+    private int x;
+    private int y;
+    private final String direction;
 
+    public Rover (int x, int y, String direction){
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
     }
     public String execute(String commands) {
-        return "1 2 N";
+
+        String[] allCommands = commands.split("");
+        for (String command: allCommands) {
+            if (command.equals("N")){
+                y++;
+            }
+            if (command.equals("S")){
+                y--;
+            }
+        }
+        return buildReturnValue();
+    }
+    
+    private String buildReturnValue(){
+        return this.x + this.y + this.direction;  //TODO - use string builder
     }
 }
--- src/test/java/mchall/RoverTest.java
+++ src/test/java/mchall/RoverTest.java
@@ -12,4 +12,44 @@ public class RoverTest {
 
         assertEquals(("1 2 N"), new Rover (1,2, "N").execute(emptyCommand));
     }
+
+    @Test
+    public void returnNorthWhenEastWithLeftCommand(){
+
+    }
+
+    @Test
+    public void returnNorthWhenWestWithRightCommand(){
+
+    }
+
+    @Test
+    public void returnEastWhenNorthWithRightCommand(){
+
+    }
+
+    @Test
+    public void returnEastWhenSouthWithLeftCommand(){
+
+    }
+
+    @Test
+    public void returnSouthWhenWestWithLeftCommand(){
+
+    }
+
+    @Test
+    public void returnSouthWhenEastWithRightCommand(){
+
+    }
+
+    @Test
+    public void returnWestWhenSouthWithRightCommand(){
+
+    }
+
+    @Test
+    public void returnWestWhenNorthWithLeftCommand(){
+
+    }
 }
