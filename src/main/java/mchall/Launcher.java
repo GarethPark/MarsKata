3a497e01ec3fd9f6dfa76a60680a16664cb21ee7
garethpark <garethpark@yahoo.com>
2021-11-27 15:08:41 +0000

    refactor Launcher

--- src/main/java/mchall/Launcher.java
+++ src/main/java/mchall/Launcher.java
@@ -6,30 +6,29 @@ import java.util.List;
 import java.util.ListIterator;
 
 public class Launcher {
-    //Change method name - it's doing more than 1 thing - what does it actually do?
+    private String finalOutput="";
+
     public String launchRovers(String upperRightCoordinates, String commands){
-        String output=""; //change this name
 
         Terrain terrain = createTerrain(upperRightCoordinates);
         List<String> commandList = createCommandList(commands);
-        ListIterator cmdItr = commandList.listIterator();
+        ListIterator commandIterator = commandList.listIterator();
 
-        while(cmdItr.hasNext()) {
-            String initialPosition = cmdItr.next().toString();
+        while(commandIterator.hasNext()) {
+            String initialPosition = commandIterator.next().toString();
 
-            int initialX = getInitialX(initialPosition);
-            int initialY = getInitialY(initialPosition);
-            String initialDirection = getInitialDirection(initialPosition);
+            RoverController controller = new RoverController(getInitialX(initialPosition),
+                                getInitialY(initialPosition),
+                                getInitialDirection(initialPosition),
+                                terrain);
 
-            RoverController controller = new RoverController(initialX, initialY, initialDirection, terrain);
-            output = output.trim() + " " + controller.executeCommands(cmdItr.next().toString()).trim();
+            AppendRoverOutputToFinalOutput(controller.executeCommands(commandIterator.next().toString()).trim());
         }
-        return output.trim();
+        return finalOutput.trim();
     }
     private List<String> createCommandList(String commands){
         return new ArrayList<String>(Arrays.asList(commands.split(" ")));
     }
-    //Make names more generic
     private int getInitialX(String initialPosition){
         return Character.getNumericValue(initialPosition.charAt(0));
     }
@@ -41,5 +40,13 @@ public class Launcher {
     }
     private Terrain createTerrain(String  upperRightCoordinates){
         return new Terrain(this.getInitialX(upperRightCoordinates), this.getInitialY(upperRightCoordinates));
+    }
+    private void AppendRoverOutputToFinalOutput(String roverOutput){
+        if(this.finalOutput == ""){
+            this.finalOutput = roverOutput;
+        }
+        else{
+            this.finalOutput+= " " + roverOutput;
+        }
     }
 }
